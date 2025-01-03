ec9ec69572b76618b8441a5bfcf75e848f094041
Srujana C Bhandary <135805635+shcarlet@users.noreply.github.com>
2025-01-03 17:59:29 +0530

    Create RansomeNote.py

---
+++ RansomeNote.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        mag_hash = {}
+
+        for c in magazine:
+            mag_hash[c]= 1+mag_hash.get(c,0)
+
+        for c in ransomNote:
+            if c not in mag_hash or mag_hash[c]<=0:
+                return False
+            mag_hash[c]-=1
+        return True

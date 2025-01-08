72415683cda2fd082256939004dc14cd9b4d1754
Srujana C Bhandary <135805635+shcarlet@users.noreply.github.com>
2025-01-08 22:11:25 +0530

    Create 3sum.py

---
+++ 3sum.py
@@ -0,0 +1,21 @@
+class Solution: 
+    def threeSum(self, nums):
+        res=[]
+        nums.sort()
+        for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            j=i+1
+            k=len(nums)-1
+            while j<k:
+                total=nums[i]+nums[j]+nums[k]
+                if total>0:
+                    k=k-1
+                elif total<0:
+                    j=j+1
+                else:
+                    res.append([nums[i],nums[j],nums[k]])
+                    j=j+1
+                    while(nums[j]==nums[j-1] and j<k):
+                        j=j+1
+        return res

7e4d85cea810d8f4614a97dc95f3642dd3e72936
Srujana C Bhandary <135805635+shcarlet@users.noreply.github.com>
2025-01-15 23:06:13 +0530

    Create mergesortll.py

---
+++ mergesortll.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        temp = ListNode()
+        cur = temp
+        while list1 and list2:
+            if list1.val>list2.val:
+                cur.next=list2
+                list2=list2.next
+            else:
+                cur.next=list1
+                list1=list1.next
+            cur=cur.next
+        if list1:
+            cur.next=list1
+        else:
+            cur.next=list2
+        return temp.next
+        

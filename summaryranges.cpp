b4023c5213ca99e819a008f6af9e1014ed1e9bc0
Srujana C Bhandary <135805635+shcarlet@users.noreply.github.com>
2025-02-10 00:19:31 +0530

    Create summaryranges.cpp

---
+++ summaryranges.cpp
@@ -0,0 +1,39 @@
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+     
+        vector<string>res;
+        
+        if(nums.empty())
+        {
+            return res;
+        }
+        int start=nums[0];
+        for(int i=1;i<=nums.size();++i)
+        {
+            if(i==nums.size() || nums[i]!=nums[i-1]+1)
+            {
+              if(start==nums[i-1])
+               { 
+                res.push_back(to_string(start));
+                }
+            
+            else
+            {
+                res.push_back(to_string(start)+"->"+to_string(nums[i-1]));
+            }
+            
+            if(i<nums.size())
+            {
+                start=nums[i];
+            }
+            }
+        }
+        return res;
+        
+    }
+};

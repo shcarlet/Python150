b91a567194e82f87d0b93e6d60482f7d92333ae5
Srujana C Bhandary <135805635+shcarlet@users.noreply.github.com>
2025-02-23 22:32:56 +0530

    Create mergeinterval.cpp

---
+++ mergeinterval.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int k=0;
+        sort(intervals.begin(),intervals.end());
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(intervals[k][1]>=intervals[i][0])
+            {
+                intervals[k][1]=max(intervals[k][1],intervals[i][1]);
+            }
+            else
+            {
+                k++;
+                intervals[k]=intervals[i];
+            }
+        }
+        intervals.resize(k+1);
+        return intervals;
+    }
+};

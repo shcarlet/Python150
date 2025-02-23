284e4f06c56c3f635402fca81edead09940645ca
Srujana C Bhandary <135805635+shcarlet@users.noreply.github.com>
2025-02-24 00:32:37 +0530

    Create duplicates.py

---
+++ duplicates.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        set1=set(nums)
+        if len(set1)==len(nums):
+            return False
+        else:
+            return True
+        
